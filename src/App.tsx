731d059e9c70835076cbc2f349b54d99f44888f2
eduardo-wrkt <eduardo@worktables.io>
2024-10-31 12:04:00 -0300

    Revert "Initial commit"
    This reverts commit f33a197ed7b7054d21b39daf49c67454610607d0.

--- src/App.tsx
+++ src/App.tsx
@@ -4,83 +4,27 @@ import { Loader, Text } from 'monday-ui-react-core';
 import { monday } from './lib/monday';
 import { useContext } from './store/context';
 
-import DocumentUpload from './DocumentUpload';
-
 interface MondayContext {
   data: {
     boardId: number;
     theme: string;
-    itemIds?: string[];
   };
 }
 
 export function App() {
   const [isLoading, setIsLoading] = useState(true);
-  const [items, setItems] = useState<any[]>([]);
+
   const context = useContext();
 
   async function getPortalContext() {
     setIsLoading(true);
 
-    const response = await monday.get('context');
-    console.log(response);
-    await context.setTheme(response.data.theme);
-    await context.setBoardId(response.data.boardId);
+    const response = (await monday.get('context')) as MondayContext;
 
-    if (response.data.boardId) {
-      await fetchItemsData(response.data.itemIds);
-    } else {
-      monday.execute('notice', 'Error');
-    }
-    //monday.listen('itemIds', fetchItemsData as any);
-  }
+    context.setTheme(response.data.theme);
+    context.setBoardId(response.data.boardId);
 
-  async function fetchItemsData() {
-    try {
-      console.log('✨', context.boardId);
-
-      const query = `query {
-      boards(ids: 7751362097) {
-        description
-        name
-        items_page(limit:500) {
-          items {
-            id
-            name
-            column_values {
-              id
-              value
-              type
-              text
-            }
-            subitems {
-              id
-              name
-              column_values {
-                id
-                value
-                type
-                text
-              }
-            }
-          }
-        }
-      }
-      }`;
-      console.log('✨✨', query);
-      const response: any = await monday.api(query);
-      //setItems(response.data.boards[0].items_page.items);
-      context.setBoardData({
-        name: response.data.boards[0].name,
-        description: response.data.boards[0].description,
-      });
-      context.setItemData(response.data.boards[0].items_page.items);
-
-      console.log(response);
-      setIsLoading(false);
-    } catch (error) {
-      console.error('Error fetching items:', error);
-    }
+    setIsLoading(false);
   }
 
   useEffect(() => {
@@ -89,14 +33,14 @@ export function App() {
 
   return !isLoading ? (
     <div className={`min-h-screen w-full ${context.theme}-app-theme`}>
-      <DocumentUpload />
+      <Text>This is a cool board view template!</Text>
     </div>
   ) : (
     <div
       className={`flex min-h-screen w-full flex-col items-center justify-center gap-2 ${context.theme}-app-theme`}
     >
       <Loader size={Loader.sizes.MEDIUM} color={Loader.colors.DARK} />
-      <Text>Loading board data...</Text>
+      <Text>Loading...</Text>
     </div>
   );
 }
--- src/store/context.ts
+++ src/store/context.ts
@@ -3,28 +3,18 @@ import { create } from 'zustand';
 interface ContextState {
   boardId: number | null;
   theme: string;
-  boardData: { name: string; description: string };
-  itemData: any[];
+
   setBoardId: (boardId: number) => void;
   setTheme: (theme: string) => void;
-  setBoardData: (boardData: { name: string; description: string }) => void;
-  setItemData: (itemData: any) => void;
 }
 
 export const useContext = create<ContextState>((set) => {
   return {
     boardId: null,
     theme: 'basic',
-    boardData: { name: '', description: '' },
-    itemData: [],
+
     setBoardId: (boardId: number) => {
       set({ boardId });
-    },
-    setBoardData: (boardData: { name: string; description: string }) => {
-      set({ boardData });
-    },
-    setItemData: (itemData: any) => {
-      set({ itemData });
     },
 
     setTheme: (theme: string) => {
